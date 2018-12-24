d17561e03a2d7bc3cc1d57dc99dd1bd7d62c1a88
MbahJoey <39154494+MbahJoey@users.noreply.github.com>
2018-12-25 00:04:05 +0200

    Controllers, Models and Migrations

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/title', 'TitlesController');
+Route::resource('/author', 'AuthorsController');
+Route::resource('/type', 'TypesController');
